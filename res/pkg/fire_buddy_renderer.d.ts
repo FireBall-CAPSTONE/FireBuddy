89733b37bfb9d0fa1818fa121ea5cc58b12f40c7
hrice9 <hrice9@uncc.edu>
2022-10-21 15:02:18 -0400

    add 3D :D

---
+++ res/pkg/fire_buddy_renderer.d.ts
@@ -0,0 +1,53 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export class App {
+  free(): void;
+/**
+* @param {string} element_id
+*/
+  constructor(element_id: string);
+/**
+* @param {number} delta_time
+* @param {number} canvas_height
+* @param {number} canvas_width
+*/
+  update(delta_time: number, canvas_height: number, canvas_width: number): void;
+/**
+*/
+  render(): void;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_app_free: (a: number) => void;
+  readonly app_new: (a: number, b: number) => number;
+  readonly app_update: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly app_render: (a: number, b: number) => void;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_exn_store: (a: number) => void;
+}
+
+/**
+* Synchronously compiles the given `bytes` and instantiates the WebAssembly module.
+*
+* @param {BufferSource} bytes
+*
+* @returns {InitOutput}
+*/
+export function initSync(bytes: BufferSource): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ res/renderer.js
@@ -0,0 +1,44 @@
+import init, {App} from "./pkg/fire_buddy_renderer.js";
+
+// let timer = document.getElementById('fps');
+let canvas = document.getElementById('canvas');
+
+async function run() {
+    await init();
+
+
+    // start();
+
+    let app = new App('canvas');
+    
+    var lastDrawTime = Date.now();
+    var delta = 0.001;
+
+    // app.render();
+
+    function render() {
+        
+        // canvas.width = window.innerWidth;
+        // canvas.height = window.innerHeight;
+
+
+        canvas.width = (window.innerWidth * .95) - 15;
+        canvas.height = window.innerHeight * .80;
+        
+        app.update(delta/1000.0, canvas.height, canvas.width);
+        app.render();
+
+        const currTime = Date.now();
+        delta = ((currTime - lastDrawTime));
+
+        
+        lastDrawTime = currTime;
+        // timer.innerHTML = `${delta} ms`;
+
+        requestAnimationFrame(render);
+    }
+
+    render();
+}
+
+await run();
