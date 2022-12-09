881c7e022ecb54124df231919286047af3675f48
hrice9 <hrice9@uncc.edu>
2022-12-09 14:34:12 -0500

    use actual data in globe view

--- public/res/renderer.js
+++ public/res/renderer.js
@@ -15,10 +15,10 @@ async function run() {
     var delta = 0.001;
 
     // app.render();
-    for( let i = 0; i < 100; i ++ ) {
-        //               // Lat                      // Long                      // Altitude
-        app.add_fireball((Math.random() * 180) - 90, (Math.random() * 360) - 180, (Math.random() * .035) + .005);
-    }
+    // for( let i = 0; i < 100; i ++ ) {
+    //     //               // Lat                      // Long                      // Altitude
+    //     app.add_fireball((Math.random() * 180) - 90, (Math.random() * 360) - 180, (Math.random() * .035) + .005);
+    // }
 
     function render() {
         
@@ -40,6 +40,40 @@ async function run() {
 
         requestAnimationFrame(render);
     }
+
+    // Populate the data
+    $.ajax({
+        url: 'https://ssd-api.jpl.nasa.gov/fireball.api',
+        type: "GET",
+        dataType: "json",
+        data: {
+        },
+        success: function (result) {
+            for( let i = 0; i < result.data.length; i++ )
+            {
+                console.log(result.data[i]);
+                let lat = result.data[i][3];
+                let south = result.data[i][4] == 'S';
+                let lon = result.data[i][5];
+                let west = result.data[i][6] == 'W';
+                let alt = result.data[i][8];
+                if(alt != null)
+                {
+                    let smod = -1;
+                    let wmod = -1;
+                    if (south)
+                        smod = 1;
+                    if (west)
+                        wmod = 1;
+                    app.add_fireball(lat * wmod, lon * smod, alt * 0.01);
+                }
+                    
+            }
+        },
+        error: function () {
+            // do nothing for now
+        }
+    });
 
     render();
 }
