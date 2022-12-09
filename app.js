02d5f52e80f61cca41128c6d8e22e2db010f5148
Kwong-Yuankit <79934646+Kwong-Yuankit@users.noreply.github.com>
2022-12-09 14:07:15 -0500

    server-side firedata page fix

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('tiny'));
 app.use(methodOverride('_method'));
+app.use(express.static(__dirname + "/public/javascript"));
 
 
 //set up routes
